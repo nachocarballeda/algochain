f6dc9fd373481d4c7ae30d86ff57ffaf314c155f
ralvarez <ralvarezr@fi.uba.ar>
2020-11-02 02:39:54 -0300

    header methods added

--- include/header.h
+++ include/header.h
@@ -2,19 +2,30 @@
 #define _HEADER_H_
 
 #include <cstdlib>
+#include <string>
 
 using namespace std;
 
 class Header
 {
-    public:
-        Header();
-        char* getPrevBlock(void);
     private:
-        char _prev_block[32];
-        char _txns_hash[32];
+        string _prev_block = "ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff";
+        string _txns_hash;
         size_t _bits;
         size_t _nonce;
+    public:
+        Header();                               //Constructor simple.
+        Header(size_t &);                       //Constructor con bits.
+        Header(const Header &);                 //Constructor copia.
+        ~Header();                              //Destructor.
+        string getPrevBlock(void) const;        //Devuelve el _prev_block
+        size_t getBits(void) const;             
+        size_t getNonce(void) const;
+        void setBits(size_t);
+        friend std::ostream &operator<<(std::ostream &, const Header &);
+    private:
+        void setTxnsHash();
+    
 };
 
 #endif /** _HEADER_H_ */

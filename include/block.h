8e5eba858b0b1c7f1479b61f58d59daa047d5613
ralvarez <ralvarezr@fi.uba.ar>
2020-11-05 23:12:21 -0300

    Methods added

--- include/block.h
+++ include/block.h
@@ -6,11 +6,27 @@
 
 class Block
 {
-    public:
-        Block();
     private:
         Header _header;
         Body _body;
+
+    public:
+        Block();
+        Block(const Header &, const Body &);
+        Block(const Block &);
+        ~Block();
+
+        void setHeader(const Header &);
+        void setBody(const Body &);
+        Header const &getHeader() const;
+        Body const &getBody() const;
+        void updateTxnsHash();
+        void proofOfWork();
+
+
+        friend std::ostream &operator<<(std::ostream &, const Block &);
+
+    
 };
 
 #endif /** _BLOCK_H_ */
--- src/block.cpp
+++ src/block.cpp
@@ -1,10 +1,80 @@
 #include "../include/block.h"
+#include "../include/body.h"
+#include "../include/sha256.h"
 
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 Block::Block()
+{   
+}
+
+Block::Block(const Header &h, const Body &b){
+    _header = h;
+    _body = b;
+}
+
+Block::Block(const Block &b){
+    _header = b._header;
+    _body = b._body;
+}
+
+Block::~Block()
 {
-    cout << "Block Constructor was called." << endl;
+}
+
+
+
+void Block::setHeader(const Header &h){
+    _header = h;
+}
+
+void Block::setBody(const Body &b){
+    _body = b;
+}
+
+Header const & Block::getHeader() const{
+    return _header;
+}
+
+Body const & Block::getBody() const{
+    return _body;
+}
+
+void Block::updateTxnsHash(){
+    //Falta funcion para concatenar las transacciones.
+
+
+    /*
+    
+    
+    string s = _body.concatenacion();
+    _header.setTxnsHash(sha256(sha256(s)));
+
+    //funcion de prueba
+    cout << _header.getTxnsHash() << endl;
+    */
+
+   cout << "Falta Implementar la concantenacion de las transacciones." << endl;
+}
+
+void Block::proofOfWork(){
+
+    string s = _header.cat();
+
+    cout << s;
+
+    string h = sha256(sha256(s));
+
+    cout << h << endl;
+
+    //Falta probar la dificultad e ir incrimentando el nonce.
+}
+
+
+ostream &operator<<(ostream &os, const Block &b){
+    return os << b._header;
+               // << b._body            FALTA IMPLEMENTAR ESTA FUNCION
 }

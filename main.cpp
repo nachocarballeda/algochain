8bdcf688ebcfd985a50321e6214c6aff304cb11f
ralvarez <ralvarezr@fi.uba.ar>
2020-11-05 23:09:51 -0300

    Block tests added

--- main.cpp
+++ main.cpp
@@ -5,8 +5,8 @@
 
 //#include "include/body.h"
 #include "include/header.h"
-//#include "include/block.h"
-//#include "include/sha256.h"
+#include "include/block.h"
+#include "include/sha256.h"
 //#include "include/cmdline.h"
 
 #define MSG_ERR_OPEN_FILE "Error al abrir el archivo "
@@ -47,7 +47,10 @@ int main(int argc, char *argv[])
     string output = sha256(input);
     cout << "input: " << input << endl;
     cout << "output: " << output << endl;*/
-	
+
+	cout << "PRUEBAS HEADER" << endl;
+
+
 	size_t b = 5;
 	Header h(b);
 
@@ -67,10 +70,43 @@ int main(int argc, char *argv[])
 	cout << h2.getPrevBlock() << endl; 
 
 	Header h3 = h2;
-
+	h3.incrementNonce();
 	cout << h3 << endl;
 
 	h3.setTxnsHash("Hola");
 	cout << h3.getTxnsHash() << endl;
+
+	cout << "PRUEBAS BLOQUE" << endl;
+
+	Block b1;
+
+	b1.setHeader(h1);
+
+	cout << b1 << endl;
+
+	Header h4 = b1.getHeader();
+
+	cout << h4 << endl;
+
+
+	b1.updateTxnsHash();
+
+
+	//Falta la funcion de concatencion de transacciones el updateTxnsHash.
+	//Falta el proof of work verificando la dificultad. Ambas deberian estar en privadas del bloque.
+
+	string s = h4.cat();
+
+	cout << s;
+
+	Header h5;
+	h5.setBits(3);
+	h5.setTxnsHash("155dc94b29dce95bb2f940cdd2d7e0bce66dca9370c3ed96d50a30b3d84f8c4c");
+
+	Block b2;
+
+	b2.setHeader(h5);
+	b2.proofOfWork();
+	//Falta probar la dificultad del proof
 
 }

4c9c4c8865bdef5c9794454ec2d6ebd01a1d177a
ralvarez <ralvarezr@fi.uba.ar>
2020-11-07 00:53:21 -0300

    ProofOfWork method implemented

--- src/block.cpp
+++ src/block.cpp
@@ -4,6 +4,7 @@
 
 #include <iostream>
 #include <string>
+#include <bitset>
 
 using namespace std;
 
@@ -53,6 +54,8 @@ void Block::updateTxnsHash(){
     string s = _body.concatenacion();
     _header.setTxnsHash(sha256(sha256(s)));
 
+    //aqui va el proofofwork, no en el main.
+
     //funcion de prueba
     cout << _header.getTxnsHash() << endl;
     */
@@ -62,15 +65,86 @@ void Block::updateTxnsHash(){
 
 void Block::proofOfWork(){
 
-    string s = _header.cat();
+    size_t d = _header.getBits();       //Obtengo la dificultad
 
-    cout << s;
+    if(d == 0)                          //Si es 0, es indistinto el hash.
+        return ;
+    
+    size_t conteo = 0;
+    bitset<4> c;
+    bool flag = true;
 
-    string h = sha256(sha256(s));
+    while(flag){
 
-    cout << h << endl;
+        string s = _header.cat();
 
-    //Falta probar la dificultad e ir incrimentando el nonce.
+        cout << s;
+
+        string h = sha256(sha256(s));
+
+        cout << "hash" << endl << h << endl;
+
+        //La cantidad de bits nulos del hash debe ser igual o mayor a la dificultad.
+        
+        for(size_t j = 0; j < 32; j++){        //Reviso los 32 bytes del hash.
+
+            if((h[j] - 48) > 48){              //Si esta entre a-f 
+
+                c = (h[j] - 87);    
+
+            }else{                             //Si esta entre 0-9
+
+                c = (h[j] - 48);                
+            }
+
+           // cout << "byte = " << c << endl;
+
+            size_t i = 0;
+
+            /*
+                Pruebo que cada bit sea 0. El bit c[0] = LSb y c[3] = MSb
+            */
+            for(; i < 4; i++){         
+
+                if(c[3 - i] != 0){
+                   // cout << "i" << i << endl;
+                    break;
+                }
+                    
+
+                conteo++;
+               // cout << "adentro" << conteo << endl;
+            }
+            
+            if(i == 0){
+               _header.incrementNonce();
+               conteo = 0;
+              // cout << "IGual a 0" << endl;
+                break; 
+            }
+
+            if((conteo < d) && ((conteo % 4) != 0)){
+                _header.incrementNonce();
+               // cout << conteo << "menor" << endl;
+                conteo = 0;
+                break;
+            }
+                
+
+            if(conteo >= d){        //Si el conteo es mayor o igual a la dificultad
+                 flag = false;
+                 //cout << conteo << "sali" << endl;
+                 break;              //Salgo del ciclo.
+            }         
+
+           // cout << "Conteo afuera" << conteo << endl;   
+    
+        }
+
+
+    }
+    
+    
 }
 
 

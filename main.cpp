c97a668c9a4718da23ad5d835bb2728f89894f6d
Nacho <carballeda.ignacio@gmail.com>
2020-11-11 18:00:30 -0300

    change tab to 4 spaces in main.cpp

--- main.cpp
+++ main.cpp
@@ -14,12 +14,12 @@
 #include "include/arghandler.h"
 
 option_t options[] = {
-	{1, "d", "difficulty", "1", opt_set_difficulty, OPT_MANDATORY},
-	{1, "o", "output", "-", opt_set_output, OPT_DEFAULT},
-	{1, "i", "input", "-", opt_set_input, OPT_DEFAULT},
-	{
-		0,
-	}};
+    {1, "d", "difficulty", "1", opt_set_difficulty, OPT_MANDATORY},
+    {1, "o", "output", "-", opt_set_output, OPT_DEFAULT},
+    {1, "i", "input", "-", opt_set_input, OPT_DEFAULT},
+    {
+        0,
+    }};
 
 using namespace std;
 
@@ -28,11 +28,11 @@ int main(int argc, char *argv[])
     string _input_file;
     string _output_file;
     size_t _difficulty;
-	cmdline cmdl(options);
-	cmdl.parse(argc, argv);
+    cmdline cmdl(options);
+    cmdl.parse(argc, argv);
     tie(_input_file, _output_file, _difficulty) = opt_get_values();
-	Block block0(_input_file);
-	block0.setDifficulty(_difficulty);
-	block0.updateTxnsHash();
-	block0.writeToFile(_output_file);
+    Block block0(_input_file);
+    block0.setDifficulty(_difficulty);
+    block0.updateTxnsHash();
+    block0.writeToFile(_output_file);
 }
